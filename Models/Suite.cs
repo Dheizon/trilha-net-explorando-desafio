c0c3f0697a7df1d1f59d16b663ed7803d29f85d2
Dheizon Gonçalves <dheizonx@gmail.com>
2022-10-04 00:06:22 -0300

    Resolucao desafio

--- Models/Suite.cs
+++ Models/Suite.cs
@@ -11,6 +11,30 @@ namespace DesafioProjetoHospedagem.Models
             ValorDiaria = valorDiaria;
         }
 
+        public Suite(int opcao)
+        {
+            switch (opcao)
+            {
+                case 1:
+                    TipoSuite = "Menos premium";
+                    Capacidade = 2;
+                    ValorDiaria = 30;
+                    break;
+                case 2:
+                    TipoSuite = "Quase Premium";
+                    Capacidade = 4;
+                    ValorDiaria = 60;
+                    break;
+                case 3:
+                    TipoSuite = "Premium";
+                    Capacidade = 6;
+                    ValorDiaria = 100;
+                    break;
+                default:
+                    break;
+            }
+        }
+
         public string TipoSuite { get; set; }
         public int Capacidade { get; set; }
         public decimal ValorDiaria { get; set; }
--- Program.cs
+++ Program.cs
@@ -3,23 +3,85 @@ using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
 
+Console.WriteLine("Seja bem vindo ao sistema de HOSPEDAGEM!");
+
+bool exibirMenu = true;
+
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+// Realiza o loop do menu
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar hospedes");
+    Console.WriteLine("2 - Efetuar reserva");
+    Console.WriteLine("3 - Encerrar");
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+    switch (Console.ReadLine())
+    {
+        case "1":
+            Console.WriteLine("Informe o nome do hospede:");
+            string nome = Console.ReadLine();
+            if (nome == null)
+            {
+                Console.WriteLine("Nome invalido");
+                break;
+            }
+            else
+            {
+                Pessoa pessoa = new Pessoa(nome: nome);
+                hospedes.Add(pessoa);
+            }
+            break;
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+        case "2":
+            Console.WriteLine("Digite a suite para hospedagem:");
+            Console.WriteLine("1 - Menos Premium");
+            Console.WriteLine("2 - Quase Premuim");
+            Console.WriteLine("3 - Premium");
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+            string tipoSuite = Console.ReadLine();
+            int intTipoSuite = 0;
+            bool validaOpcaoSuite = int.TryParse(tipoSuite, out intTipoSuite);
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+            if (validaOpcaoSuite)
+            {
+                // Cria a suíte
+                Suite suite = new Suite(intTipoSuite);
+
+                // Cria uma nova reserva, passando a suíte e os hóspedes
+                Console.WriteLine("Informe a quantidade de dias:");
+                string qtdeDias = Console.ReadLine();
+
+                Reserva reserva = new Reserva(diasReservados: Convert.ToInt32(qtdeDias));
+                reserva.CadastrarSuite(suite);
+
+                if (hospedes.Count > 0)
+                    reserva.CadastrarHospedes(hospedes);
+
+                // Exibe a quantidade de hóspedes e o valor da diária
+                Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+                Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria().ToString("C2")}");
+            }
+            else
+            {
+                Console.WriteLine("Opcao de suite invalida");
+            }
+
+            break;
+
+        case "3":
+            exibirMenu = false;
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
+
+Console.WriteLine("O programa se encerrou");
